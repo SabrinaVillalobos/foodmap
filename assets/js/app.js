8d1f26e7db2de0c2a392599fe5d54602c68cc6d1
SabrinaVillalobos <wafflish@gmail.com>
2017-12-12 21:58:58 -0300

    filter done

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,9 +3,37 @@ $(document).ready(function(){
  $('.spcont').delay(3000).fadeOut("slow");
 
 
-
-
-
+$(function() {
+    $('#preciodiv').hide();
+    $('#alphord').hide();
+    $('#valdiv').hide();
+    $('#filtrores').change(function(){
+        if($('#filtrores').val() == 'price'){
+            $('#preciodiv').show(); 
+            $('.foodimages').hide();
+        } else {
+            $('#preciodiv').hide(); 
+        }
+        if($('#filtrores').val() == 'locales abiertos'){
+            $('#abiertodiv').show(); 
+            $('.foodimages').hide();
+        } else {
+            $('#abiertodiv').hide(); 
+        }
+        if($('#filtrores').val() == 'valoracion'){
+            $('#valdiv').show(); 
+            $('.foodimages').hide();
+        } else {
+            $('#valdiv').hide(); 
+        }
+         if($('#filtrores').val() == 'orden alfabetico'){
+            $('#alphord').show(); 
+            $('.foodimages').hide();
+        } else {
+            $('#alphord').hide(); 
+        }
+    });
+});
 
 });
 
